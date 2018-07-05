f1ab9e1274edc172fb7604abf6099789892dde30
Nathan Heffley <nathanheffley@gmail.com>
2018-07-05 16:04:47 -0400

    Move CSRF Token into App object

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -15,10 +15,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * a simple convenience so we don't have to attach every token manually.
  */
 
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+if (App.csrfToken) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = App.csrfToken;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name') }} - @yield('title')</title>
 
         <link rel="stylesheet" href="/css/app.css">
+
+        <script>
+            window.App = {
+                csrfToken: '{{ csrf_token() }}',
+            }
+        </script>
     </head>
     <body class="bg-grey-lighter">
         <div id="app">
